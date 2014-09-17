3f48edcd4d6513c6f07e98c63795676a9aeb80c0
Mark Ryall <mark@ryall.name>
2014-09-17 23:53:56 +1000

    added form to page and uglify for js compression

--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 - npm install --save browserify
 - npm install --save react
 - npm install --save reactify
+- npm install --save uglifyjs
+- browserify -t reactify src/main.js -o scripts.js
 - browserify -t reactify src/main.js -o scripts.js
 - python -m SimpleHTTPServer
 - [open](http://localhost:8000)
--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,16 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  _submit: function(e) {
+    e.preventDefault();
+  },
+
   render: function() {
-    return <div>Hello World</div>
+    return (
+      <form onSubmit={ this._submit }>
+        <input type="text" id="username" name="username" />
+        <input type="submit" value="search" />
+      </form>
+    );
   }
 });
