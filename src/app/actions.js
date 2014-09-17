97b05f4bd4d3aba7df9af886f4faf4ec10240647
Mark Ryall <mark@ryall.name>
2014-09-18 00:06:32 +1000

    added change event on form input to update state and form submit to call action

---
+++ src/app/actions.js
@@ -0,0 +1,7 @@
+"use strict";
+
+module.exports = {
+  retrieveTracks: function(username) {
+    console.log("retrieving tracks for " + username);
+  }
+}
--- src/app/index.js
+++ src/app/index.js
@@ -1,17 +1,29 @@
 /** @jsx React.DOM */
 "use strict";
 
-var React = require('react');
+var React   = require('react');
+var actions = require('./actions');
 
 module.exports = React.createClass({
-  _submit: function(e) {
+  getInitialState: function() {
+    return {
+      name: ''
+    }
+  },
+
+  _submitted: function(e) {
     e.preventDefault();
+    actions.retrieveTracks(this.state.name);
+  },
+
+  _nameChanged: function(e) {
+    this.setState({ name: e.target.value });
   },
 
   render: function() {
     return (
-      <form onSubmit={ this._submit }>
-        <input type="text" id="username" name="username" />
+      <form onSubmit={ this._submitted }>
+        <input type="text" id="username" name="username" onChange={ this._nameChanged } />
         <input type="submit" value="search" />
       </form>
     );
