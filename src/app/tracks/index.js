26cdf46c13b935ebed4818b4ea7200915617615b
Mark Ryall <mark@ryall.name>
2014-09-19 15:39:35 +1000

    added display of each track

--- src/app/tracks/index.js
+++ src/app/tracks/index.js
@@ -1,11 +1,20 @@
 /** @jsx React.DOM */
 "use strict";
 
-var React   = require('react');
+var React = require('react');
+var _     = require('lodash');
 
 module.exports = React.createClass({
   render: function() {
     var tracks = this.props.data;
-    return <div>{tracks.length} tracks</div>
+
+    function buildTrack(data) {
+      return <div>{ data.name }</div>
+    }
+
+    return <div>
+      <span>{tracks.length} tracks</span>
+      { _.map(tracks, buildTrack) }
+    </div>
   }
 });
